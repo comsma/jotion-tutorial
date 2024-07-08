95d422d2cd7273daca0dcc830e3c4226d325bd40
Collin Smallegan <cmsmallegan@comsma.com>
2024-07-08 11:50:44 -0500

    updated marketing background bug

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -6,7 +6,7 @@ import { Footer } from "@/app/(marketing)/_components/footer";
 
 export default function MarketingPage() {
   return (
-    <div className={"min-h-full flex flex-col"}>
+    <div className={"min-h-full dark:bg-[#1f1f1f] flex flex-col"}>
       <div
         className={
           "flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"
